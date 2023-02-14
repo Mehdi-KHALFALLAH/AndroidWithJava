6d9967fa05fe0703d9f1e6ff77463b920b32431d
Mehdi Khalfallah <contact.mehdi.khalfallah@gmail.com>
2023-02-14 14:17:39 +0100

    second commit

--- app/src/main/java/com/example/androidwithjava/MainActivity.java
+++ app/src/main/java/com/example/androidwithjava/MainActivity.java
@@ -13,13 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        char s = 'k';
+        System.out.println(s);
 
 
     }
     public void onBtnClick (View view) {
         TextView txtHello =findViewById(R.id.textView);
         EditText edtTxtName = findViewById(R.id.edittext1) ;
-        txtHello.setText("Hello " + edtTxtName.getText().toString());
-
+        String string = edtTxtName.getText().toString() ;
+        if (string.equals("")) {
+            System.out.println("nothign is here");
+            txtHello.setText("Hello ");
+        } else {
+            txtHello.setText("Hello " + edtTxtName.getText().toString());
+        }
     }
 }
